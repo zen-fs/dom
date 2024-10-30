5d47ebe78efe53e2508f30bd5b38c225e770db54
James Prevett <jp@jamespre.dev>
2024-10-30 15:51:47 -0500

    Added `WebAccess` tests Added tsx dev. dependency

---
+++ tests/setup-access.ts
@@ -0,0 +1,14 @@
+import * as fsAccess from 'file-system-access';
+import adapter from 'file-system-access/lib/adapters/memory.js';
+Object.assign(globalThis, fsAccess);
+
+import { configureSingle } from '@zenfs/core';
+import { WebAccess } from '../src/access.js';
+import { copy, data } from '@zenfs/core/tests/setup/common.js';
+
+await configureSingle({
+	backend: WebAccess,
+	handle: await fsAccess.getOriginPrivateDirectory(adapter),
+});
+
+copy(data);
