e87f5c0456741a44c7d73d200127d4bb7df0a522
James Prevett <jp@jamespre.dev>
2026-07-25 12:42:53 -0500

    Updated `@zenfs/core`, test config

--- tests/setup-access.ts
+++ tests/setup-access.ts
@@ -1,4 +1,5 @@
 import { configureSingle, sync } from '@zenfs/core';
+import type { TestFlag, TestFlagState } from '@zenfs/core/tests/common.js';
 import { copyAsync, data } from '@zenfs/core/tests/setup.js';
 import { afterEach } from 'node:test';
 import { WebAccess } from '../src/access.js';
@@ -8,3 +9,8 @@ await configureSingle({ backend: WebAccess, handle });
 await copyAsync(data);
 
 afterEach(sync);
+
+export const flags: Partial<Record<TestFlag, TestFlagState>> = {
+	// The File System Access API has no concept of hard links
+	links: false,
+};
